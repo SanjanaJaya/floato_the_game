442cb8fb23a626c8417f04e2e217b69d7deefc49
Sanjana Jayasooriya <135824601+SanjanaJaya@users.noreply.github.com>
2025-04-07 02:25:37 +0530

    background added

--- lib/game.dart
+++ lib/game.dart
@@ -1,17 +1,22 @@
 import 'dart:async';
-
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:floato_the_game/components/background.dart';
 import 'package:floato_the_game/components/rocket.dart';
 
 class floato extends FlameGame with TapDetector{
 
   late Rocket rocket;
+  late Background background;
 
   @override
   FutureOr<void> onLoad() {
+    background = Background(size);
+    add(background);
+
     rocket = Rocket();
     add(rocket);
+
   }
 
   @override
