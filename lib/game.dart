c9c6ec420975e488f93a722d9b4acef9f3f12dc5
Sanjana Jayasooriya <135824601+SanjanaJaya@users.noreply.github.com>
2025-04-07 09:26:04 +0530

    restart button added

--- lib/game.dart
+++ lib/game.dart
@@ -4,6 +4,8 @@ import 'package:flame/game.dart';
 import 'package:floato_the_game/components/background.dart';
 import 'package:floato_the_game/components/rocket.dart';
 import 'package:floato_the_game/components/ground.dart';
+import 'package:floato_the_game/constants.dart';
+import 'package:flutter/material.dart';
 
 class floato extends FlameGame with TapDetector, HasCollisionDetection{
 
@@ -37,9 +39,32 @@ class floato extends FlameGame with TapDetector, HasCollisionDetection{
 
     isGameOver = true;
     pauseEngine();
+
+    showDialog(
+      context: buildContext!,
+      builder: (context) => AlertDialog(
+        title: const Text("Game Over Hutto"),
+        actions: [
+          TextButton(
+            onPressed: (){
+
+              Navigator.pop(context);
+
+              resetGame();
+            },
+            child: const Text("Restart"),
+          )
+        ],
+      ),
+    );
   }
 
-
+  void resetGame(){
+    rocket.position = Vector2(rocketStartX, rocketStartY);
+    rocket.velocity = 0;
+    isGameOver = false;
+    resumeEngine();
+  }
 
 }
 
